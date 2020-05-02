64e1da4f8ee2d5db8a9ebe4b1aacb0427acec6d8
Joe Neeman <joeneeman@gmail.com>
2020-05-02 14:26:40 -0500

    Cleanups and documentation for timeline.

--- scribble/src/widgets/timeline.rs
+++ scribble/src/widgets/timeline.rs
@@ -2,8 +2,9 @@ use druid::kurbo::{BezPath, Line, Vec2};
 use druid::theme;
 use druid::widget::{Controller, Scroll};
 use druid::{
-    BoxConstraints, Color, Command, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx,
-    PaintCtx, Point, Rect, RenderContext, Size, UpdateCtx, Widget, WidgetExt, WidgetPod,
+    Affine, BoxConstraints, Color, Command, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle,
+    LifeCycleCtx, PaintCtx, Point, Rect, RenderContext, Size, UpdateCtx, Widget, WidgetExt,
+    WidgetPod,
 };
 use std::collections::HashMap;
 
@@ -32,35 +33,97 @@ const SNIPPET_WAVEFORM_COLOR: Color = Color::rgb8(0x33, 0x33, 0x99);
 
 const MARK_COLOR: Color = Color::rgb8(0x33, 0x33, 0x99);
 
+/// Converts from a time interval to a width in pixels.
 fn pix_width(d: Diff) -> f64 {
     d.as_micros() as f64 * PIXELS_PER_USEC
 }
 
+/// Converts from a width in pixels to a time interval.
 fn width_pix(p: f64) -> Diff {
     Diff::from_micros((p / PIXELS_PER_USEC) as i64)
 }
 
+/// Converts from a time instant to an x-position in pixels.
 fn pix_x(t: Time) -> f64 {
     t.as_micros() as f64 * PIXELS_PER_USEC
 }
 
+/// Converts from an x-position in pixels to a time instant.
 fn x_pix(p: f64) -> Time {
     Time::from_micros((p / PIXELS_PER_USEC) as i64)
 }
 
+/// The id of a snippet (either a drawing snippet or an audio snippet).
 #[derive(Clone, Copy, Eq, Hash, PartialEq)]
 enum Id {
     Drawing(SnippetId),
     Audio(AudioSnippetId),
 }
 
+/// The cached "waveform" of an audio snippet.
+#[derive(Clone, Data)]
+struct AudioWaveform {
+    // The shape of the waveform. This is rendered with respect to a height
+    // going from -1 to 1.
+    wave: BezPath,
+}
+
+/// The data of a snippet (either a drawing snippet or an audio snippet).
 #[derive(Clone, Data)]
 enum Snip {
     Drawing(SnippetData),
     Audio(AudioSnippetData),
 }
 
+impl AudioWaveform {
+    fn from_audio(data: AudioSnippetData) -> AudioWaveform {
+        // Converts a PCM sample to a y coordinate. This could use some more
+        // thought and/or testing. Audio samples seem to never get anywhere near
+        // i16::MAX, and so a linear scale up to i16:MAX ends up not drawing
+        // anything. Taking a log evens out the magnitudes a lot, and discarding
+        // the lower values seems to improve the appearance too, but 10.0 is
+        // completely arbitrary.
+        let audio_height = |x: i16| -> f64 {
+            let x = x as f64;
+            let sign = x.signum();
+            let x = (x.abs() - 10.0).max(1.0);
+
+            // This gives a height between -1 and 1.
+            sign as f64 * x.log(std::i16::MAX as f64)
+        };
+
+        let width = pix_width(data.end_time() - data.start_time());
+        let pix_per_sample = 5;
+        let buf = data.buf();
+        let mut mags = Vec::with_capacity((width as usize) / pix_per_sample);
+        let mut path = BezPath::new();
+        path.move_to((0.0, 0.0));
+        for p in (0..(width as usize)).step_by(pix_per_sample) {
+            let start_time = x_pix(p as f64) - time::ZERO;
+            let end_time = x_pix((p + pix_per_sample) as f64) - time::ZERO;
+            let start_idx =
+                (start_time.as_audio_idx(crate::audio::SAMPLE_RATE) as usize).min(buf.len());
+            let end_idx =
+                (end_time.as_audio_idx(crate::audio::SAMPLE_RATE) as usize).min(buf.len());
+            let sub_buf = &buf[start_idx..end_idx];
+
+            let mag = (sub_buf.iter().cloned().max().unwrap_or(0)
+                - sub_buf.iter().cloned().min().unwrap_or(0))
+                / 2;
+            path.line_to((p as f64, audio_height(mag)));
+            mags.push((p, mag));
+        }
+
+        for (p, mag) in mags.into_iter().rev() {
+            path.line_to((p as f64, -audio_height(mag)));
+        }
+        path.close_path();
+        AudioWaveform { wave: path }
+    }
+}
+
 impl Snip {
+    /// At what time does this snippet start?
     fn start_time(&self) -> Time {
         match self {
             Snip::Audio(s) => s.start_time(),
@@ -68,6 +131,8 @@ impl Snip {
         }
     }
 
+    /// At what time does this snippet end? Returns `None` if the snippet never
+    /// ends.
     fn end_time(&self) -> Option<Time> {
         match self {
             Snip::Audio(s) => Some(s.end_time()),
@@ -75,6 +140,7 @@ impl Snip {
         }
     }
 
+    /// Returns the list of times at which this snippet was lerped.
     fn inner_lerp_times(&self) -> Vec<Diff> {
         match self {
             Snip::Audio(_) => Vec::new(),
@@ -99,75 +165,12 @@ impl Snip {
             }
         }
     }
-
-    fn render_interior(&self, ctx: &mut PaintCtx, width: f64, height: f64) {
-        match self {
-            Snip::Audio(data) => {
-                // Converts a PCM sample to a y coordinate.
-                let audio_height = |x: i16| -> f64 {
-                    let sign = x.signum();
-                    let x = x.abs() as f64 + 1.0;
-
-                    // This gives a height between -1 and 1.
-                    let y = sign as f64 * x.log(std::i16::MAX as f64);
-                    // Now convert it to graphical y coord.
-                    height / 2.0 + height * y / 2.0
-                };
-
-                let pix_per_sample = 5;
-                let buf = data.buf();
-                // TODO: shouldn't be recalculating this every paint.
-                let mut mins = Vec::with_capacity((width as usize) / pix_per_sample);
-                let mut path = BezPath::new();
-                path.move_to((0.0, 0.0));
-                for p in (0..(width as usize)).step_by(pix_per_sample) {
-                    let start_time = x_pix(p as f64) - time::ZERO;
-                    let end_time = x_pix((p + pix_per_sample) as f64) - time::ZERO;
-                    let start_idx = (start_time.as_audio_idx(crate::audio::SAMPLE_RATE) as usize)
-                        .min(buf.len());
-                    let end_idx =
-                        (end_time.as_audio_idx(crate::audio::SAMPLE_RATE) as usize).min(buf.len());
-                    let sub_buf = &buf[start_idx..end_idx];
-
-                    let max = sub_buf.iter().cloned().max().unwrap_or(0);
-                    path.line_to((p as f64, audio_height(max)));
-                    mins.push((p, sub_buf.iter().cloned().min().unwrap_or(0)));
-                }
-
-                for (p, min) in mins.into_iter().rev() {
-                    path.line_to((p as f64, audio_height(min)));
-                }
-                path.close_path();
-                ctx.fill(path, &SNIPPET_WAVEFORM_COLOR);
-            }
-            Snip::Drawing(data) => {
-                // Draw the span of the edited region.
-                let end = data.end_time().unwrap_or(Time::from_micros(std::i64::MAX));
-                let last_draw_time = data.last_draw_time().min(end);
-                let draw_width = pix_width(last_draw_time - data.start_time());
-                let color = Color::rgb8(0, 0, 0);
-                ctx.stroke(
-                    Line::new((0.0, height / 2.0), (draw_width, height / 2.0)),
-                    &color,
-                    1.0,
-                );
-                ctx.stroke(
-                    Line::new((draw_width, height * 0.25), (draw_width, height * 0.75)),
-                    &color,
-                    1.0,
-                );
-
-                // Draw the lerp lines.
-                for t in self.inner_lerp_times() {
-                    let x = pix_width(t);
-                    ctx.stroke(Line::new((x, 0.0), (x, height)), &SNIPPET_STROKE_COLOR, 1.0);
-                }
-            }
-        }
-    }
 }
 
+/// The main timeline widget.
 struct TimelineInner {
+    // The timeline is organized in rows, and this map associates each id to a
+    // row (with 0 being the topmost row).
     snippet_offsets: HashMap<Id, usize>,
     num_rows: usize,
     children: HashMap<Id, WidgetPod<AppState, TimelineSnippet>>,
@@ -175,13 +178,23 @@ struct TimelineInner {
 
 pub fn make_timeline() -> impl Widget<AppState> {
     let inner = TimelineInner::default();
-    Scroll::new(inner).controller(TimelineScrollController)
+    Scroll::new(inner)
+        .controller(TimelineScrollController)
+        // This is a hack to hide the scrollbars. Hopefully in the future druid will
+        // support this directly.
+        .env_scope(|env, _data| {
+            env.set(theme::SCROLLBAR_WIDTH, 0.0);
+            env.set(theme::SCROLLBAR_EDGE_WIDTH, 0.0);
+        })
 }
 
+/// A widget wrapping the timeline's `Scroll` that updates the scroll to follow
+/// the cursor.
 struct TimelineScrollController;
 
 impl<W: Widget<AppState>> Controller<AppState, Scroll<AppState, W>> for TimelineScrollController {
     // TODO: we should be able to do this using `update` instead of relying on a command
+    // The problem is that `UpdateCtx` has no `size()`.
     fn event(
         &mut self,
         child: &mut Scroll<AppState, W>,
@@ -233,7 +246,8 @@ impl Default for TimelineInner {
 }
 
 impl TimelineInner {
-    fn recalculate_snippet_offsets(&mut self, snippets: &SnippetsData, audio: &AudioSnippetsData) {
+    // Recreates the child widgets, and organizes them into rows so that they don't overlap.
+    fn recreate_children(&mut self, snippets: &SnippetsData, audio: &AudioSnippetsData) {
         let draw_offsets = snippet_layout::layout(snippets.snippets());
         let audio_offsets = snippet_layout::layout(audio.snippets());
         self.num_rows = (draw_offsets.num_rows + audio_offsets.num_rows).max(MIN_NUM_ROWS);
@@ -244,19 +258,29 @@ impl TimelineInner {
             let id = Id::Drawing(id);
             self.snippet_offsets.insert(id, offset);
             self.children
-                .insert(id, WidgetPod::new(TimelineSnippet { id }));
+                .insert(id, WidgetPod::new(TimelineSnippet { id, wave: None }));
         }
         for (&id, &offset) in &audio_offsets.positions {
+            let audio_data = audio.snippet(id);
             let id = Id::Audio(id);
             self.snippet_offsets.insert(id, self.num_rows - offset - 1);
-            self.children
-                .insert(id, WidgetPod::new(TimelineSnippet { id }));
+            self.children.insert(
+                id,
+                WidgetPod::new(TimelineSnippet {
+                    id,
+                    wave: Some(AudioWaveform::from_audio(audio_data.clone())),
+                }),
+            );
         }
     }
 }
 
+/// A widget representing a single snippet (audio or drawing) in the timeline.
 struct TimelineSnippet {
+    // The id of the snippet that this widget represents.
     id: Id,
+    // If the snippet is an audio snippet, a precalculated waveform.
+    wave: Option<AudioWaveform>,
 }
 
 impl TimelineSnippet {
@@ -285,12 +309,55 @@ impl TimelineSnippet {
                     DRAW_SNIPPET_COLOR
                 }
             }
-            // FIXME: make a audio selected color too
             Id::Audio(id) => {
                 if data.scribble.selected_snippet == id.into() {
                     AUDIO_SNIPPET_SELECTED_COLOR
                 } else {
                     AUDIO_SNIPPET_COLOR
+                }
+            }
+        }
+    }
+
+    /// Draws the "interior" of the snippet (i.e., everything but the bounding rect).
+    fn render_interior(&self, ctx: &mut PaintCtx, snip: &Snip, _width: f64, height: f64) {
+        match snip {
+            Snip::Audio(_data) => {
+                ctx.with_save(|ctx| {
+                    // The precomputed waveform is based on a vertical scale of
+                    // [-1, 1], so transform it to [0, height]
+                    ctx.transform(
+                        Affine::translate((0.0, height / 2.0))
+                            * Affine::scale_non_uniform(1.0, height / 2.0),
+                    );
+                    let wave = self
+                        .wave
+                        .as_ref()
+                        .expect("audio snippet should have a cached waveform");
+                    ctx.fill(&wave.wave, &SNIPPET_WAVEFORM_COLOR);
+                });
+            }
+            Snip::Drawing(data) => {
+                // Draw the span of the edited region.
+                let end = data.end_time().unwrap_or(Time::from_micros(std::i64::MAX));
+                let last_draw_time = data.last_draw_time().min(end);
+                let draw_width = pix_width(last_draw_time - data.start_time());
+                let color = Color::rgb8(0, 0, 0);
+                ctx.stroke(
+                    Line::new((0.0, height / 2.0), (draw_width, height / 2.0)),
+                    &color,
+                    1.0,
+                );
+                ctx.stroke(
+                    Line::new((draw_width, height * 0.25), (draw_width, height * 0.75)),
+                    &color,
+                    1.0,
+                );
+
+                // Draw the lerp lines.
+                for t in snip.inner_lerp_times() {
+                    let x = pix_width(t);
+                    ctx.stroke(Line::new((x, 0.0), (x, height)), &SNIPPET_STROKE_COLOR, 1.0);
                 }
             }
         }
@@ -325,6 +392,9 @@ impl Widget<AppState> for TimelineSnippet {
         let snip = self.snip(data);
         let old_snip = self.snip(old_data);
         if !snip.same(&old_snip) {
+            if let Snip::Audio(data) = snip {
+                self.wave = Some(AudioWaveform::from_audio(data));
+            }
             ctx.request_paint();
         }
 
@@ -362,9 +432,20 @@ impl Widget<AppState> for TimelineSnippet {
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, env: &Env) {
         let snippet = self.snip(data);
-        let width = self.width(data).min(10000.0); // FIXME: there are bugs drawing infinite rects.
+        let width = self.width(data);
         let height = SNIPPET_HEIGHT;
+        let radius = env.get(theme::BUTTON_BORDER_RADIUS);
+
+        // Logically, untruncated snippets have infinite width. But druid
+        // doesn't support drawing rectangles of infinite width, so we truncate
+        // our rectangle to be just a little bit bigger than the drawing region.
+        let bounding_rect = ctx
+            .region()
+            .to_rect()
+            .inflate(radius + 1.0, std::f64::INFINITY);
         let rect = Rect::from_origin_size(Point::ZERO, (width, height))
+            .inset(-SNIPPET_STROKE_THICKNESS / 2.0)
+            .intersect(bounding_rect)
             .to_rounded_rect(env.get(theme::BUTTON_BORDER_RADIUS));
         let stroke_color = if ctx.is_hot() {
             &SNIPPET_STROKE_COLOR
@@ -373,10 +454,13 @@ impl Widget<AppState> for TimelineSnippet {
         };
         let fill_color = self.fill_color(data);
 
-        ctx.fill(&rect, &fill_color);
-        ctx.stroke(&rect, stroke_color, SNIPPET_STROKE_THICKNESS);
-
-        snippet.render_interior(ctx, width, height);
+        ctx.with_save(|ctx| {
+            let clip = ctx.region().to_rect();
+            ctx.clip(clip);
+            ctx.fill(&rect, &fill_color);
+            ctx.stroke(&rect, stroke_color, SNIPPET_STROKE_THICKNESS);
+            self.render_interior(ctx, &snippet, width, height);
+        });
     }
 }
 
@@ -421,10 +505,7 @@ impl Widget<AppState> for TimelineInner {
                 .same(&old_data.scribble.audio_snippets)
         {
             ctx.request_layout();
-            self.recalculate_snippet_offsets(
-                &data.scribble.snippets,
-                &data.scribble.audio_snippets,
-            );
+            self.recreate_children(&data.scribble.snippets, &data.scribble.audio_snippets);
             ctx.children_changed();
         }
         if old_data.time() != data.time() || old_data.scribble.mark != data.scribble.mark {
@@ -439,10 +520,7 @@ impl Widget<AppState> for TimelineInner {
         match event {
             LifeCycle::WidgetAdded => {
                 ctx.request_layout();
-                self.recalculate_snippet_offsets(
-                    &data.scribble.snippets,
-                    &data.scribble.audio_snippets,
-                );
+                self.recreate_children(&data.scribble.snippets, &data.scribble.audio_snippets);
                 ctx.children_changed();
             }
             _ => {}
@@ -469,21 +547,14 @@ impl Widget<AppState> for TimelineInner {
         }
 
         let height = SNIPPET_HEIGHT * self.num_rows as f64;
-        let last_draw_time = data.scribble.snippets.last_draw_time();
-        let last_audio_time = data.scribble.audio_snippets.end_time();
-        let time_width = last_draw_time
-            .max(last_audio_time)
-            .max(data.time())
-            // TODO: this is to work around some issues with filling the scrollable area.
-            .max(Time::from_micros(59_000_000))
-            + Diff::from_micros(1_000_000);
-        let width = pix_x(time_width);
-        bc.constrain((width, height))
+        bc.constrain((std::f64::INFINITY, height))
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, env: &Env) {
+        // Note that the width here may well be infinite. Intersecting with the
+        // paint region will prevent us from trying to fill an infinite rect.
         let size = ctx.size();
-        let rect = Rect::from_origin_size(Point::ZERO, size);
+        let rect = Rect::from_origin_size(Point::ZERO, size).intersect(ctx.region().to_rect());
         ctx.fill(rect, &TIMELINE_BG_COLOR);
 
         for child in self.children.values_mut() {
